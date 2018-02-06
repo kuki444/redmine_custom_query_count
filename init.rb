8b40182f8145034a9f6f969e389ce01cffaead19
kuki-n7 <kuki-n7@itec.hankyu-hanshin.co.jp>
2018-02-06 11:00:04 +0900

    init add url and change name

--- init.rb
+++ init.rb
@@ -7,9 +7,10 @@ end
 
 Rails.application.config.i18n.load_path += Dir["#{File.dirname(__FILE__)}/config/locales/*.{rb,yml}"]
 
-Redmine::Plugin.register :redmine_custom_query_status do
+Redmine::Plugin.register :redmine_custom_query_count do
   name 'Redmine Custom Query Count plugin'
   author 'Norio Kuki'
   description 'チケット一覧のカスタムクエリ一覧にクエリに該当するチケット数を表示する'
   version '0.1.0'
+  url 'https://github.com/kuki444/redmine_custom_query_count'
 end
